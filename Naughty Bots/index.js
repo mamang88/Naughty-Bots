402f91ea2554384455a046cdc3ea26321c067171
mamang88 <47385520+mamang88@users.noreply.github.com>
2019-02-06 20:09:35 +0700

    masukin awal file
    awal rilis

---
+++ Naughty Bots/index.js
@@ -0,0 +1,10 @@
+const Discord=include("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready",async ()=> {
+	console.log(`[Start] ${new Date()}`);
+	console.log(`${bot.user.username} is online`);
+	bot.user.setActivity("From tohir Computer", {type: "WATCHING"});
+});
+
+bot.login("NTMzODA4NjQyMjkwNjE0Mjgy.DzxZyg._al-LfwujpOUY_h4A6I8azz1nbI");
